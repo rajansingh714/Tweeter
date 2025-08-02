0770876c91447dfc694de3ab4f431858c1fd9129
Your Name <rajansingh70707@gmail.com>
2025-08-02 11:38:01 +0530

    added get comment

--- src/index.js
+++ src/index.js
@@ -1,31 +1,23 @@
-const express = require('express');
+const express = require("express");
 
-
-const connect = require('./config/serverConfig');
-const Tweet = require('./models/tweet');
-const tweet = require('./repository/tweet-repository');
-
+const connect = require("./config/serverConfig");
+const Comment = require("./models/comments");
+const TweetRepository = require("./repository/tweet-repository");
 
 const app = express();
 
-
-
 const serverSetup = () => {
+  app.listen(3000, async () => {
+    console.log(`server is running on ${3000}`);
+    await connect();
+    console.log("mongoo db  coneect");
 
-    app.listen(3000, async() => {
-          console.log(`server is running on ${3000}`);
-          await connect();
-          console.log('mongoose is coneect');
-
-          const tweetRepo = new tweet();
-          const result = await tweetRepo.update('688ca25323fd6ab033b0b44b', {
-            content: 'my tweet is working'
-          });
-          console.log(result);
-    });
-}
-
+    const tweetRepo = new TweetRepository();
+    const getComment = await tweetRepo.getWithComments(
+      "688d9cf220b01310c144cdd5"
+    );
+    console.log(getComment);
+  });
+};
 
 serverSetup();
-
- 
