ab7980db214db2f05ffb5fc30d13ce996e6c5744
Your Name <rajansingh70707@gmail.com>
2025-08-03 13:47:03 +0530

    added in models how to use virtul

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const connect = require("./config/serverConfig");
 const Comment = require("./models/comments");
+const Tweet = require("./models/tweet");
 const TweetRepository = require("./repository/tweet-repository");
 
 const app = express();
@@ -10,15 +11,15 @@ const serverSetup = () => {
   app.listen(3000, async () => {
     console.log(`server is running on ${3000}`);
     await connect();
-    console.log("mongoo db  coneect");
+    console.log("mongoo db  connect");
 
     const tweetRepo = new TweetRepository();
-    // const getComment = await tweetRepo.getWithComments(
-    //   "688d9cf220b01310c144cdd5"
-    // );
-    // console.log(getComment);
+
     const result = await tweetRepo.getAll(2, 4);
-    console.log(result);
+    // console.log(result);
+    // const tweet = result[0].toJSON();
+    // console.log(tweet);
+    console.log(result[0].contentWithEmail);
   });
 };
 
--- src/models/tweet.js
+++ src/models/tweet.js
@@ -1,22 +1,31 @@
 const mongoose = require("mongoose");
 
-const tweetSchema = new mongoose.Schema({
+const tweetSchema = new mongoose.Schema(
+  {
     content: {
-        type: String,
-        require: true,
+      type: String,
+      require: true,
     },
     userEmail: {
-        type: String
+      type: String,
     },
     comments: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Comment'
-        }
-    ]
-}, {timestamps: true});
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
-const Tweet = mongoose.model('Tweet', tweetSchema);
+tweetSchema.virtual("contentWithEmail").get(function process() {
+  return `${this.content} \n Created by: ${this.userEmail}`;
+});
+
+const Tweet = mongoose.model("Tweet", tweetSchema);
 module.exports = Tweet;
-
-
