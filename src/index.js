554b90897e52d780ef4600619ba450c287ad048f
Your Name <rajansingh70707@gmail.com>
2025-08-10 11:05:35 +0530

    signup with encrypted password

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import bodyParser from "body-parser";
 import { connect } from "./config/serverConfig.js";
 
 import apiRoutes from "./routes/index.js";
-import { UserRepository, TweetRepository } from "./repository/index.js";
-import LikeService from "./services/like-service.js";
 
 const app = express();
 
@@ -17,17 +15,4 @@ app.listen(3000, async () => {
   console.log(`server is running on ${3000}`);
   await connect();
   console.log("mongoo db  connected");
-
-  // const userRepo = new UserRepository();
-  // const tweetRepo = new TweetRepository();
-  // const tweets = await tweetRepo.getAll(0, 10);
-  // const users = await userRepo.getAll();
-  // const result = await userRepo.create({
-  //   email: "rajanSingh@gmail.com",
-  //   password: 741258,
-  //   name: "Rajan",
-  // });
-
-  // const likeService = new LikeService();
-  // await likeService.toggleLike(tweets[0].id, "Tweet", users[0].id);
 });
